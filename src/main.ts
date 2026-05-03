849c402579a45818c50e6542f05d13e973d4de1a
Julián Tobito <julianmontoua@gmail.com>
2026-05-03 00:03:44 -0500

    feat: add Dockerfile and .dockerignore; update CORS origin to use environment variable

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   app.useGlobalFilters(new AllExceptionsFilter());
   let port: number = Number(process.env.PORT);
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
     credentials: true,
   });
 
